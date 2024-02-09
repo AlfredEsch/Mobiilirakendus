731f48fcbb2232ec9dd2c8748501b6eb1e04c81f
AlfredEsch <91603138+AlfredEsch@users.noreply.github.com>
2024-02-09 13:30:28 +0200

    Create Splash screen

---
+++ src/screens/auth/Splash/Index.js
@@ -0,0 +1,25 @@
+import react from "react";
+import {Text,View,Image} from "react-native"
+import Button from "/Users/student/Mobiilirakendus/src/components/Button/Index.js";
+import { styles } from "./styles";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+
+
+const Splash = () => {
+    return(
+        <View style={styles.container}>
+            <Image reziseMode="contain" style={styles.image} source={require("/Users/student/Mobiilirakendus/src/assets/splash_image.png")}/>    
+        <View style={styles.titleContainer}>    
+            <Text style={styles.title}>You'll Find </Text>
+            <Text style={[styles.title, styles.innerTitle]}> All you need </Text>
+            <Text style={styles.title}>Here!</Text>
+        </View>
+            <Button title= "Sign Up"/>
+
+            <Pressable hitSlop={20}>
+                <Text style={styles.footerText}>Sign In</Text>
+            </Pressable>
+        </View>
+    )
+}
+export default Splash 
